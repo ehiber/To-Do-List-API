a06e54ec8339e1109cd3db85ac586fc7cd17f2a5
ehiber <ehiber5@hotmail.com>
2020-01-29 21:44:04 +0000

    agregando tablas y consultas

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -32,10 +32,135 @@ def sitemap():
 def handle_hello():
 
     response_body = {
-        "hello": "world"
+        "task": "Alguna tarea",
+        "done": "true"
     }
 
     return jsonify(response_body), 200
+
+@app.route('/todos/user/<username>', methods=['POST', 'GET','PUT','DELETE'])
+def handle_user_task(username):
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+    #Chequeando si el usuario existe
+    requesting_user = User.query.filter_by(username=username).all()
+    if len(requesting_user) > 0:
+        username_id = requesting_user[0].id
+    else : 
+        username_id = None 
+
+    if request.method == 'GET':
+        
+        print("Hello, GET!")
+        if username_id is not None:
+            # Existe el usuario, retornamos la lista de tareas...
+            print("Usuario Existe")
+            user_tasks_list = Task.query.filter_by(user_id=username_id).all()
+            response_body = []
+            for task in user_tasks_list:
+                response_body.append(task.serialize())
+            status_code = 200
+        else :
+            print("Usuario No Existe")
+            response_body = {
+                "status" : "HTTP_404_NOT_FOUND. Usuario no existe"
+            }
+            status_code = 404
+        
+    elif request.method == 'POST':
+        
+        print("Creando Usuario con una tarea")
+
+        if username_id:
+            
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. Usuario ya existe..."
+            }
+            status_code = 400
+        
+        elif request.data != []:
+            
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. Datos inesperados para crear usuario"
+            }
+            status_code = 400
+
+        else:
+            # username not in use, create user with sample task...
+            print("Creando usuario con una tarea")
+            new_user = User()
+            new_user.username = username
+            db.session.add(new_user)
+            sample_task = Task("sample task", username_id)
+            db.session.add(sample_todo)
+            db.session.commit()
+            response_body = {
+                "status": "Ok"
+            }
+            status_code = 200
+
+    elif request.method == 'PUT':
+        
+        # El usuario actualiza la lista completa de tareas, se chequea si existe primerp...
+        print(f"updating full list for {username}")
+
+        if username_id:
+            # Usuario existe, actualizamos toda la lista...
+            # Eliminando tareas existentes...
+            Task.query.filter_by(user_id=username_id).delete()
+            new_tasks = json.loads(request.data)
+            
+            for task in new_tasks:
+                
+                new_task = Task(task["label"], username_id)
+                db.session.add(new_task)
+
+            db.session.commit()
+            result = f"A list with {len(new_tasks)} todos was succesfully saved"
+            response_body = {
+                "result": result,
+            }
+            status_code = 200
+
+        else: 
+            # Usuario no exite...
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. No se puede actualizar la lista de un usario inexistente..."
+            }
+            status_code = 400
+
+    elif request.method == "DELETE":
+        # Usuario quiere eliminar si registro y lista...
+        if username_id:
+            # Usuario existe, eliminando registros...
+
+            Task.query.filter_by(user_id=username_id).delete()
+            User.get_by_id(int(username_id)).delete()
+            db.session.commit()
+            response_body = {
+                "result": "ok",
+                "status": "HTTP_204_NO_CONTENT. Usuarios Y Tareas Eliminad@s."
+            }
+            status_code = 204
+        
+        else:
+            # user does not exist, this is a no go...
+            response_body = {
+                "status": "HTTP_400_BAD_REQUEST. Cannot delete a non existing user..."
+            }
+            status_code = 400
+
+    else:
+        response_body = "Metodo no esta listo todavia"
+        status_code = 501
+
+    return make_response(
+        jsonify(response_body),
+        status_code,
+        headers
+    )
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    task = db.relationship('task')
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "username": self.username,
+            }
+    
+    @staticmethod
+    def get_by_id(id):
+        return User.query.get(id)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(240), nullable=False)
+    done = db.Column(db.Boolean, default= False)
+    user = db.relationship('user')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Tasks (Label: %r, Done: %r>' % self.label , self.done
+
+    def serialize(self):
+        return {
+            "label": self.label,
+            "done": self.done
+        }
